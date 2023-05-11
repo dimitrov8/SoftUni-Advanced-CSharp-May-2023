66227d5a09711a99ed0a174d50cb95f9446eba7a
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-05-11 09:57:41 +0300

    Upload

---
+++ 06. Defining Classes/Lab/01. Car/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car
+            {
+                Make = "VW",
+                Model = "MK3",
+                Year = 1992
+            };
+            Console.WriteLine(car);
+        }
+    }
+}
