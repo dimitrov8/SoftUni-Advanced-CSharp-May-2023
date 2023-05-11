fe0a8f51ae4b8d07497e5e6531a59b1ae1cbd8d9
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-05-11 09:13:18 +0300

    Upload

---
+++ 06. Defining Classes/Lab/01. Car/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+
+        public string Make
+        {
+            get =>  make;
+            init => make = value;
+        }
+
+        public string Model
+        {
+            get => model;
+            init => model = value;
+        }
+
+        public int Year
+        {
+            get => year;
+            init => year = value;
+        }
+        
+
+        public override string ToString()
+        {
+            return $"Make: {Make}{Environment.NewLine}Model: {Model}{Environment.NewLine}Year: {Year}";
+        }
+    }
+}
