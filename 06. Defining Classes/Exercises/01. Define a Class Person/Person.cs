69606e74190c88672f0fcee1af7742232b3ca9d5
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-05-15 17:56:33 +0300

    Upload

---
+++ 06. Defining Classes/Exercises/01. Define a Class Person/Person.cs
@@ -0,0 +1,26 @@
+﻿namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ 06. Defining Classes/Exercises/02. Creating Constructors/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>() {new Person("Peter", 20), new Person("George", 18), new Person("Jose", 43)};
+        }
+    }
+}
