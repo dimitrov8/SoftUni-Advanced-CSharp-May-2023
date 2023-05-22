d5e275cb0cd482180680e08b20990465337b7749
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-05-22 09:56:58 +0300

    Upload

---
+++ 07. Generics/Lab/01. Box of T/Box.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private Stack<T> box;
+
+        public Box()
+        {
+            this.box = new Stack<T>();
+        }
+
+        public void Add(T element)
+        {
+            box.Push(element);
+        }
+
+        public T Remove()
+        {
+            if (box.Count == 0)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+            return box.Pop();
+        }
+
+        public int Count => box.Count;
+    }
+}
