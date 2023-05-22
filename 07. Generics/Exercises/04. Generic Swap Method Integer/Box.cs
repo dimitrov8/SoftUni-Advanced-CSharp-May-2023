55c70ec1dd132ddc3f3b1608fa56579eb1c0248c
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-05-22 12:50:20 +0300

    Upload

---
+++ 07. Generics/Exercises/04. Generic Swap Method Integer/Box.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace GenericSwapMethodIntegers
+{
+    public class Box<T>
+    {
+            private readonly List<T> _items;
+            public int Count => _items.Count;
+
+            public Box()
+            {
+                _items = new List<T>();
+            }
+
+            public void Add(T item)
+            {
+                _items.Add(item);
+            }
+
+            public void Swap(int firstElementIndex, int secondElementIndex)
+            {
+                if (firstElementIndex >= 0 && firstElementIndex < _items.Count && secondElementIndex >= 0 && secondElementIndex < _items.Count)
+                {
+                    (_items[firstElementIndex], _items[secondElementIndex]) = (_items[secondElementIndex], _items[firstElementIndex]);
+                }
+            }
+
+            public override string ToString()
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (var item in _items)
+                {
+                    sb.AppendLine($"{typeof(T)}: {item}");
+                }
+
+                return sb.ToString().Trim();
+            }
+    }
+}
---
+++ 07. Generics/Exercises/04. Generic Swap Method Integer/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace GenericSwapMethodIntegers
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int nOfBoxes = int.Parse(Console.ReadLine()!);
+            var integerBox = new Box<int>();
+            for (int i = 0; i < nOfBoxes; i++)
+            {
+                integerBox.Add(int.Parse(Console.ReadLine()!));
+            }
+
+            if (integerBox.Count > 0)
+            {
+                int[] indexesToSwap = Console.ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                integerBox.Swap(indexesToSwap[0], indexesToSwap[1]);
+                Console.WriteLine(integerBox);
+            }
+        }
+    }
+}
