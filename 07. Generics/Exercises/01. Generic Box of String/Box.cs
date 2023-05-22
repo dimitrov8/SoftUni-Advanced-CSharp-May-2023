df310fbfdfdfc777c4452a0d447d1e4fc1559bce
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-05-22 10:58:08 +0300

    Upload

---
+++ 07. Generics/Exercises/01. Generic Box of String/Box.cs
@@ -0,0 +1,17 @@
+﻿namespace GenericBoxOfString
+{
+    public class Box<T>
+    {
+        private readonly string _text;
+
+        public Box(string text)
+        {
+            this._text = text;
+        }
+
+        public override string ToString()
+        {
+            return $"{this._text.GetType()}: {this._text}";
+        }
+    }
+}
---
+++ 07. Generics/Exercises/01. Generic Box of String/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GenericBoxOfString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int nOfStrings = int.Parse(Console.ReadLine());
+            for (int i = 0; i < nOfStrings; i++)
+            {
+                Box<string> box = new Box<string>(Console.ReadLine());
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
