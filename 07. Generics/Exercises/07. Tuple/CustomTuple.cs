711c8b0e734ce4e0cb62c90e6ec2a68184d3bec1
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-05-22 15:00:08 +0300

    Upload

---
+++ 07. Generics/Exercises/07. Tuple/CustomTuple.cs
@@ -0,0 +1,19 @@
+﻿namespace Tuple
+{
+    public class CustomTuple<T1, T2>
+    {
+        private readonly T1 _item1;
+        private readonly T2 _item2;
+        
+        public CustomTuple(T1 item1, T2 item2)
+        {
+            this._item1 = item1;
+            this._item2 = item2;
+        }
+
+        public override string ToString()
+        {
+            return $"{this._item1} -> {this._item2}";
+        }
+    }
+}
---
+++ 07. Generics/Exercises/07. Tuple/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] personNameAndAddress = Console.ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            int addressCount = personNameAndAddress.Length - 2; // Used to determinate the numbers of elements in the address that we should join
+            CustomTuple<string, string> info = new CustomTuple<string, string>(personNameAndAddress[0] + " " + personNameAndAddress[1], 
+                string.Join(" ", personNameAndAddress, 2, addressCount)); // Join the address by starting at index 2 and take all the elements from the address
+
+            string[] personBeer = Console.ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            CustomTuple<string, int> beer = new CustomTuple<string, int>(personBeer[0], int.Parse(personBeer[1]));
+
+            string[] numbers = Console.ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            CustomTuple<int, double> num = new CustomTuple<int, double>(int.Parse(numbers[0]), double.Parse(numbers[1]));
+
+            Console.WriteLine(info);
+            Console.WriteLine(beer);
+            Console.WriteLine(num);
+        }
+    }
+}
