8cdecf1a93b8d29a0a747f752789162c33ef5da4
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-05-22 12:30:46 +0300

    Upload

---
+++ 07. Generics/Exercises/04. Generic Swap Method/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenericBoxOfInteger
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int nOfIntegers = int.Parse(Console.ReadLine());
+            for (int i = 0; i < nOfIntegers; i++)
+            {
+                Console.WriteLine(new Box<int>(int.Parse(Console.ReadLine())));
+            }
+        }
+    }
+}
