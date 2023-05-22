47058af2e87f250b05e45b41b97e6ffef8703110
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-05-22 15:26:32 +0300

    Upload

---
+++ 07. Generics/Exercises/08. Threeuple/CustomThreeuple.cs
@@ -0,0 +1,21 @@
+﻿namespace Threeuple
+{
+    public class CustomThreeuple<T1, T2, T3>
+    {
+        private readonly T1 _item1;
+        private readonly T2 _item2;
+        private readonly T3 _item3;
+
+        public CustomThreeuple(T1 item1, T2 item2, T3 item3)
+        {
+            this._item1 = item1;
+            this._item2 = item2;
+            this._item3 = item3;
+        }
+
+        public override string ToString()
+        {
+            return $"{this._item1} -> {this._item2} -> {this._item3}";
+        }
+    }
+}
---
+++ 07. Generics/Exercises/08. Threeuple/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Threeuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] personNameAndTown = Console.ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            int addressCount = personNameAndTown.Length - 3;
+            var personNameAndTownInfo = new CustomThreeuple<string, string, string>(personNameAndTown[0] + " " + personNameAndTown[1], personNameAndTown[2],
+                string.Join(" ", personNameAndTown, 3, addressCount));
+
+            string[] personBeer = Console.ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            var personBeerInfo = new CustomThreeuple<string, int, bool>(personBeer[0], int.Parse(personBeer[1]), personBeer[2] == "drunk");
+            
+            string[] personBankBalance = Console.ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            var personBankBalanceInfo = new CustomThreeuple<string, double, string>(personBankBalance[0], double.Parse(personBankBalance[1]), personBankBalance[2]);
+
+            Console.WriteLine(personNameAndTownInfo);
+            Console.WriteLine(personBeerInfo);
+            Console.WriteLine(personBankBalanceInfo);
+        }
+    }
+}
