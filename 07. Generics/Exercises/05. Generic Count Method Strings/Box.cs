1fe0df3c8f3fb786cdd6a2d4c2c9a34b5f51856a
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-05-22 13:15:22 +0300

    Upload

---
+++ 07. Generics/Exercises/05. Generic Count Method Strings/Box.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCountMethodStrings
+{
+    public class Box<T> where T : IComparable<T>
+    {
+        private readonly List<T> _items;
+        public int ItemsCount => _items.Count;
+
+        public Box()
+        {
+            this._items = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            _items.Add(item);
+        }
+
+        public int Count(T inputItem)
+        {
+            int count = 0;
+            foreach (var item  in _items)
+            {
+                if (item.CompareTo(inputItem) == 1)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ 07. Generics/Exercises/05. Generic Count Method Strings/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GenericCountMethodStrings
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int nOfElements = int.Parse(Console.ReadLine()!);
+            Box<string> box = new Box<string>();
+            for (int i = 0; i < nOfElements; i++)
+            {
+                box.Add(Console.ReadLine());
+            }
+            
+            if (box.ItemsCount > 0)
+            {
+                Console.WriteLine(box.Count(Console.ReadLine()));
+            }
+        }
+    }
+}
