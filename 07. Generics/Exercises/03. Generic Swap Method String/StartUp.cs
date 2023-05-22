b785e8f95498f4931256de3e645e02c175acca55
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-05-22 11:43:16 +0300

    Upload

---
+++ 07. Generics/Exercises/03. Generic Swap Method String/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace GenericSwapMethodString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int nOfBoxes = int.Parse(Console.ReadLine()!);
+            var stringBox = new Box<string>();
+            for (int i = 0; i < nOfBoxes; i++)
+            {
+                stringBox.Add(Console.ReadLine());
+            }
+
+            if (stringBox.Count > 0)
+            {
+                int[] indexesToSwap = Console.ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                stringBox.Swap(indexesToSwap[0], indexesToSwap[1]);
+                Console.WriteLine(stringBox);
+            }
+            
+        }
+    }
+}
