25031f11b18767e510c0a35c61ade4054f9de9fc
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-05-22 11:09:35 +0300

    Upload

---
+++ 07. Generics/Exercises/02. Generic Box of Integer/Box.cs
@@ -0,0 +1,17 @@
+﻿namespace GenericBoxOfInteger
+{
+    public class Box<T>
+    {
+        private readonly int _value;
+
+        public Box(int integer)
+        {
+            this._value = integer;
+        }
+
+        public override string ToString()
+        {
+            return $"{typeof(T)}: {this._value}";
+        }
+    }
+}
