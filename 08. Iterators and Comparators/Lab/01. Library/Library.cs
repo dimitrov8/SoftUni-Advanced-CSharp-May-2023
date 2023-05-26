80d6064f4547f5fa3037e7bb15feed7d4ff6aac8
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-05-26 11:36:32 +0300

    Fix

--- 08. Iterators and Comparators/Lab/01. Library/Library.cs
+++ 08. Iterators and Comparators/Lab/01. Library/Library.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace IteratorsAndComparators
 {
-    public class Library
+    public class Library : IEnumerable<Book>
 
     {
         private List<Book> books;
@@ -11,5 +13,15 @@ namespace IteratorsAndComparators
         {
             this.books = new List<Book>(books);
         }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            foreach (var book in books)
+            {
+                yield return book;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
