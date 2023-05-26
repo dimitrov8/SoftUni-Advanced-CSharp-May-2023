028a48ad4767cb106fbd5f7a1a81286bb1c479b1
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-05-26 12:05:21 +0300

    Upload

---
+++ 08. Iterators and Comparators/Lab/03. ComparableBook/Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = authors.ToList();
+        }
+
+        public string Title { get;  set; }
+
+        public int Year { get; set; }
+
+        public List<string> Authors { get; set; }
+        
+        public int CompareTo(Book other)
+        {
+            int result = this.Year.CompareTo(other.Year);
+            if (result == 0)
+            {
+                result = String.Compare(this.Title, other.Title, StringComparison.Ordinal);
+            }
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Year}";
+        }
+    }
+}
