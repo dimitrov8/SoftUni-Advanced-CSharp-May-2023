6d923009ca46e3621685715ecb019ebcedd9543e
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-05-27 14:20:33 +0300

    Upload

---
+++ 08. Iterators and Comparators/Exercises/03. Stack/CustomStack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    public class CustomStack<T> : IEnumerable<T>
+    {
+        private List<T> customStack;
+        private int position = -1;
+
+        public CustomStack()
+        {
+            this.customStack = new List<T>();
+        }
+
+        public void Push(params T[] elements)
+        {
+            foreach (var element in elements)
+            {
+                this.customStack.Insert(++this.position, element);
+            }
+        }
+
+        public void Pop()
+        {
+            if (this.customStack.Count == 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+
+            this.customStack.RemoveAt(this.position--);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.position; i >= 0; i--)
+            {
+                yield return this.customStack[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ 08. Iterators and Comparators/Exercises/03. Stack/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command;
+            CustomStack<int> customStack = new CustomStack<int>();
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] tokens = command.Split(new[] {" ", ","}, StringSplitOptions.RemoveEmptyEntries);
+                if (tokens[0] == "Push")
+                {
+                    customStack.Push(tokens.Skip(1).Select(int.Parse).ToArray());
+                }
+                else if (tokens[0] == "Pop")
+                {
+                    try
+                    {
+                        customStack.Pop();
+                    }
+                    catch (InvalidOperationException exception)
+                    {
+                        Console.WriteLine(exception.Message);
+                    }
+                }
+            }
+
+            foreach (var element in customStack)
+            {
+                Console.WriteLine(element);
+            }
+
+            foreach (var element in customStack)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
