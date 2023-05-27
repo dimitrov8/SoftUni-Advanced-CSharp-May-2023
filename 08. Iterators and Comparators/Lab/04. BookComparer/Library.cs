a73c9ba0ca7813538c0748988a08e4c1bf069a9b
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-05-27 12:11:34 +0300

    Upload

--- 08. Iterators and Comparators/Lab/04. BookComparer/Library.cs
+++ 08. Iterators and Comparators/Lab/04. BookComparer/Library.cs
@@ -5,12 +5,11 @@ namespace IteratorsAndComparators
 {
     public class Library : IEnumerable<Book>
     {
-        private List<Book> books;
+        private SortedSet<Book> books;
 
         public Library(params Book[] books)
         {
-            this.books = new List<Book>(books);
-            this.books.Sort();
+            this.books = new SortedSet<Book>(books, new BookComparator());
         }
 
         private class LibraryIterator : IEnumerator<Book>
