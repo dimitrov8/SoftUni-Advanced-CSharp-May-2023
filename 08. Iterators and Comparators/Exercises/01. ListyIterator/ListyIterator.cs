f4dd119f15602455416e24e73c86500bdacd5535
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-05-27 13:23:41 +0300

    Upload

---
+++ 08. Iterators and Comparators/Exercises/01. ListyIterator/ListyIterator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private List<T> list;
+        private int currentIndex;
+
+        public ListyIterator(params T[] inputList)
+        {
+            this.list = new List<T>(inputList);
+        }
+
+        public bool Move()
+        {
+            if (this.currentIndex < this.list.Count - 1)
+            {
+                this.currentIndex++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasNext() => this.currentIndex + 1 < this.list.Count;
+
+        public void Print()
+        {
+            if (this.list.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            Console.WriteLine(this.list[this.currentIndex]);
+        }
+    }
+}
