5f9ad30fe59e150f0a38836ec4841b9b5c9fc988
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-05-27 15:35:22 +0300

    Upload

---
+++ 08. Iterators and Comparators/Exercises/05. Comparing Objects/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        private string name;
+        private int age;
+        private string town;
+        public Person(string name, int age, string town)
+        {
+            this.name = name;
+            this.age = age;
+            this.town = town;
+        }
+
+        public int CompareTo(Person other)
+        {
+            if (this.name.CompareTo(other.name) != 0)
+            {
+                return this.name.CompareTo(other.name);
+            }
+
+            return this.age.CompareTo(other.age) != 0 ? this.age.CompareTo(other.age) : this.town.CompareTo(other.town);
+        }
+    }
+}
---
+++ 08. Iterators and Comparators/Exercises/05. Comparing Objects/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ComparingObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            List<Person> people = new List<Person>();
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] personInfo = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                people.Add(new Person(personInfo[0], int.Parse(personInfo[1]), personInfo[2]));
+            }
+
+            int personToCompareId = int.Parse(Console.ReadLine()!) - 1;
+            Person personToCompare = people[personToCompareId];
+
+            int equalCounter = 0;
+            int nonEqualCounter = 0;
+            foreach (var person in people)
+            {
+                if (person.CompareTo(personToCompare) == 0)
+                {
+                    equalCounter++;
+                }
+                else
+                {
+                    nonEqualCounter++;
+                }
+            }
+
+            Console.WriteLine(equalCounter > 1
+                ? $"{equalCounter} {nonEqualCounter} {equalCounter + nonEqualCounter}"
+                : "No matches");
+        }
+    }
+}
