12dc8343a3a32e5b17fce624e56ad5382e7544da
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-05-30 11:31:54 +0300

    Upload

---
+++ 08. Iterators and Comparators/Exercises/07. Custom Comparator/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Linq;
+
+namespace CustomComparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+           Func<int, int, int> sortFunc = (x, y) =>
+               (x % 2 == 0 && y % 2 != 0) ? -1 :
+               (x % 2 != 0 && y % 2 == 0) ? 1 :
+               x > y ? 1 :
+               x < y ? -1 :
+               0;
+
+           Array.Sort(numbers, (x, y) => sortFunc(x, y));
+           Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
