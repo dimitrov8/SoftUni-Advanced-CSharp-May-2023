0749f753e7ed871140c64df9ff08eca101edc5f3
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-06-01 14:11:14 +0300

    Upload

---
+++ 09. Basic Algorithms/01. Recursive Array Sum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace RecursiveArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()?.Split().Select(int.Parse).ToArray();
+            Console.WriteLine(SumArray(array, 0));
+        }
+        static int SumArray(int[] array, int index)
+        {
+            if (index == array.Length - 1)
+            {
+                return array[index];
+            }
+
+            return array[index] + SumArray(array, index + 1);
+        }
+    }
+}
