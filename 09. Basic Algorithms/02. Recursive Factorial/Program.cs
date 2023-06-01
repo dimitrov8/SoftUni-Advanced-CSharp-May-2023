2c242fc9a43822e9d1c75914d60b4806fab80c67
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-06-01 14:24:27 +0300

    Upload

---
+++ 09. Basic Algorithms/02. Recursive Factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RecursiveFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine()!);
+            Console.WriteLine(GetFactorial(number));
+        }
+
+        static long GetFactorial(int number)
+        {
+            if (number == 0) return 1;
+            return number * GetFactorial(number - 1);
+        }
+    }
+}
