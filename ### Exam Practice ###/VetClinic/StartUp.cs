b4d14d7e9aaa1caa5757704426c0849a743043bb
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-06-06 02:43:56 +0300

    Not done
    91/100

---
+++ ### Exam Practice ###/VetClinic/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace VetClinic
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Clinic clinic = new Clinic(1);
+            
+            Console.WriteLine(clinic.Capacity);
+            
+            Pet dog = new Pet("Ellias", 5, "Tim");
+
+            Console.WriteLine(dog);
+
+            clinic.Add(dog);
+
+            Console.WriteLine(clinic.Remove("Ellias"));
+            Console.WriteLine(clinic.Remove("Pufa"));
+
+            Pet cat = new Pet("Bella", 2, "Mia");
+            Pet bunny = new Pet("Zak", 4, "Jon");
+
+            clinic.Add(cat);
+            clinic.Add(bunny);
+
+            Pet oldestPet = clinic.GetOldestPet();
+            Console.WriteLine(oldestPet);
+
+            Pet pet = clinic.GetPet("Bella", "Mia");
+            Console.WriteLine(pet);
+
+            Console.WriteLine(clinic.Count);
+
+            Console.WriteLine(clinic.GetStatistics());
+            Console.WriteLine(clinic.Capacity);
+        }
+    }
+}
