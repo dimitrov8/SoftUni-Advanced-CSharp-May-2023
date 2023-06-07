99e4ed92fe70d8d9f5cbc9da2be923f0e762250f
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-06-07 14:24:19 +0300

    Upload

---
+++ ### Exam Practice ###/Fishing Net/Net.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FishingNet
+{
+    public class Net
+    {
+        private readonly ICollection<Fish> fish;
+        public Net(string material, int capacity)
+        {
+            this.Material = material;
+            this.Capacity = capacity;
+            this.fish = new List<Fish>();
+        }
+        public string Material { get; set; }
+        public int Capacity { get; set; }
+
+        public int Count => this.fish.Count;
+
+        public IReadOnlyCollection<Fish> Fish => (IReadOnlyCollection<Fish>)this.fish;
+
+        public string AddFish(Fish fish)
+        {
+            if (string.IsNullOrWhiteSpace(fish.FishType) || fish.Length <= 0 || fish.Weight <= 0)
+            {
+                return "Invalid fish.";
+            }
+            
+            if (this.Fish.Count + 1 > this.Capacity)
+            {
+                return "Fishing net is full.";
+            }
+
+            this.fish.Add(fish);
+            return $"Successfully added {fish.FishType} to the fishing net.";
+        }
+
+        public bool ReleaseFish(double weight)
+        {
+                Fish fishToRelease = this.fish.FirstOrDefault(f => f.Weight == weight);
+                if (fishToRelease != null)
+                {
+                    return this.fish.Remove(fishToRelease);
+                }
+
+                return false;
+        }
+
+        public Fish GetFish(string fishType)
+        {
+            return this.fish.FirstOrDefault(f => f.FishType == fishType);
+        }
+
+        public Fish GetBiggestFish()
+        {
+            return this.fish.OrderByDescending(f => f.Length).FirstOrDefault();
+        }
+
+        public string Report()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Into the {this.Material}:");
+            foreach (var item in this.Fish.OrderByDescending(f => f.Length))
+            {
+                sb.AppendLine(item.ToString());
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ ### Exam Practice ###/Fishing Net/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FishingNet
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Net net = new Net("cast net", 2);
+
+            Fish fishOne = new Fish("salmon", 44.25, 941.15);
+            Fish fishTwo = new Fish("catfish", 105.25, 2100.15);
+            Fish fishThree = new Fish("bass", 25.25, 321.15);
+
+            Console.WriteLine(net.AddFish(fishOne));            
+            Console.WriteLine(net.AddFish(fishTwo));            
+            Console.WriteLine(net.AddFish(fishThree));
+
+            Console.WriteLine(net.ReleaseFish(2100.15));
+            
+            Console.WriteLine(net.Report());
+         
+
+        }
+    }
+}
