bca05462309ae5bf4786c176a9f7725f2cc5abb4
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-06-11 13:18:56 +0300

    Upload

---
+++ ### Exam Practice ###/Openning/Bakery.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    public class Bakery
+    {
+        public Bakery(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.Employees = new HashSet<Employee>(capacity);
+        }
+        
+        public HashSet<Employee> Employees { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+
+        public int Count => this.Employees.Count;
+
+        public void Add(Employee employee)
+        {
+            if (this.Count < this.Capacity)
+            {
+                this.Employees.Add(employee);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Employee employeeToRemove = this.Employees.FirstOrDefault(e => e.Name == name);
+            return this.Employees.Remove(employeeToRemove);
+        }
+
+        public Employee GetOldestEmployee()
+        {
+            return this.Employees.OrderByDescending(e => e.Age).FirstOrDefault();
+        }
+
+        public Employee GetEmployee(string name)
+        {
+            return this.Employees.FirstOrDefault(e => e.Name == name);
+        }
+
+        public string Report()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Employees working at Bakery {this.Name}:");
+            foreach (var employee in this.Employees)
+            {
+                sb.AppendLine(employee.ToString());
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ ### Exam Practice ###/Openning/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace BakeryOpenning
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Bakery bakery = new Bakery("Barny", 10);
+            
+            Employee employee = new Employee("Stephen", 40, "Bulgaria");
+            
+            Console.WriteLine(employee);
+
+            bakery.Add(employee);
+            Console.WriteLine(bakery.Remove("Employee name")); //false
+
+            Employee secondEmployee = new Employee("Mark", 34, "UK");
+            
+            bakery.Add(secondEmployee);
+
+            Employee oldestEmployee = bakery.GetOldestEmployee(); 
+            Employee employeeStephen = bakery.GetEmployee("Stephen"); 
+            Console.WriteLine(oldestEmployee); 
+            Console.WriteLine(employeeStephen); 
+
+            Console.WriteLine(bakery.Count); 
+
+            Console.WriteLine(bakery.Report());
+        }
+    }
+}
