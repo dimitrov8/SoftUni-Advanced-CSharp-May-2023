655087c378adbc53f4b04651f8bd6616cbd53248
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-06-11 16:48:15 +0300

    Upload

---
+++ ### Exam Practice ###/The Race/Race.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheRace
+{
+    public class Race
+    {
+        public List<Racer> Racers { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+
+        public int Count => this.Racers.Count;
+
+        public Race(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.Racers = new List<Racer>(capacity);
+        }
+
+        public void Add(Racer racer)
+        {
+            if (this.Count < this.Capacity)
+            {
+                this.Racers.Add(racer);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Racer racerToRemove = this.Racers.FirstOrDefault(r => r.Name == name);
+            return this.Racers.Remove(racerToRemove);
+        }
+
+        public Racer GetOldestRacer()
+        {
+            return this.Racers.OrderByDescending(r => r.Age).FirstOrDefault();
+        }
+
+        public Racer GetRacer(string name)
+        {
+            return this.Racers.FirstOrDefault(r => r.Name == name);
+        }
+
+        public Racer GetFastestRacer()
+        {
+            return this.Racers.OrderByDescending(r => r.Car.Speed).FirstOrDefault();
+        }
+
+        public string Report()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Racers participating at {this.Name}:");
+            foreach (var racer in this.Racers)
+            {
+                sb.AppendLine(racer.ToString());
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ ### Exam Practice ###/The Race/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TheRace
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Race race = new Race("Indianapolis 500", 10);
+
+            Car car1 = new Car("ferrari", 150);
+            Car car2 = new Car("lambo", 170);
+
+            Racer racer1 = new Racer("Stephen", 40, "Bulgaria",car1);
+
+            Console.WriteLine(racer1); 
+
+            race.Add(racer1);
+            race.Remove("Vin Benzin");
+
+            Racer racer2 = new Racer("Mark", 34, "UK",car2);
+
+            race.Add(racer2);
+
+            Racer oldestRacer = race.GetOldestRacer(); 
+            Racer racerStephen = race.GetRacer("Stephen");
+            Racer fastestRacer = race.GetFastestRacer();
+
+            Console.WriteLine(oldestRacer); 
+            Console.WriteLine(racerStephen); 
+            Console.WriteLine(fastestRacer); 
+            Console.WriteLine(race.Count);
+
+            Console.WriteLine(race.Report());
+
+        }
+    }
+}
