7da0ac3fb42748bb4cfeb39d6a4bcc60385834b4
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-06-12 17:41:52 +0300

    Upload

---
+++ ### Exam Practice ###/Cocktail Party/Cocktail.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CocktailParty
+{
+    public class Cocktail
+    {
+        public Cocktail(string name, int capacity, int maxAlcoholLevel)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.MaxAlcoholLevel = maxAlcoholLevel;
+            this.Cocktails = new HashSet<Ingredient>(capacity);
+        }
+
+        public HashSet<Ingredient> Cocktails { get; set; }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int MaxAlcoholLevel { get; set; }
+
+        public int CurrentAlcoholLevel => this.Cocktails.Sum(i => i.Alcohol);
+        
+        public void Add(Ingredient ingredient)
+        {
+            if (this.Capacity > this.Cocktails.Count && !this.Cocktails.Contains(ingredient))
+            {
+                this.Cocktails.Add(ingredient);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Ingredient ingredientToRemove = this.Cocktails.FirstOrDefault(i => i.Name == name);
+            return this.Cocktails.Remove(ingredientToRemove);
+        }
+
+        public Ingredient FindIngredient(string name)
+        {
+            return this.Cocktails.FirstOrDefault(x => x.Name == name);
+        }
+
+        public Ingredient GetMostAlcoholicIngredient()
+        {
+            return this.Cocktails.OrderByDescending(i => i.Alcohol).FirstOrDefault();
+        }
+
+        public string Report()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Cocktail: {this.Name} - Current Alcohol Level: {this.CurrentAlcoholLevel}");
+            foreach (var cocktail in this.Cocktails)
+            {
+                sb.AppendLine(cocktail.ToString());
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ ### Exam Practice ###/Cocktail Party/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CocktailParty
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Cocktail cocktail = new Cocktail("Pina Colada", 3, 10);
+            Ingredient rum = new Ingredient("Rum", 2, 3);
+            Console.WriteLine(rum.ToString());
+            cocktail.Add(rum);
+            Console.WriteLine(cocktail.Remove("Rum")); 
+            Ingredient vodka = new Ingredient("Vodka", 2, 5);
+            Ingredient milk = new Ingredient("Milk", 0, 5);
+            cocktail.Add(vodka);
+            cocktail.Add(milk);
+            Console.WriteLine(cocktail.GetMostAlcoholicIngredient());
+            Console.WriteLine(cocktail.CurrentAlcoholLevel);
+            Console.WriteLine(cocktail.Report());
+
+        }
+    }
+}
