2f9b7ba954a36016993d38f4d2bb75f739ae2a5f
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-06-15 13:30:24 +0300

    Upload

---
+++ ### Exam Practice ###/Clothes Magazine/Magazine.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClothesMagazine
+{
+    public class Magazine
+    {
+        public Magazine(string type, int capacity)
+        {
+            this.Type = type;
+            this.Capacity = capacity;
+            this.Clothes = new List<Cloth>(capacity);
+        }
+
+        public string Type { get; set; }
+        public int Capacity { get; set; }
+        public List<Cloth> Clothes { get; set; }
+
+        public int GetClothCount() => this.Clothes.Count;
+
+        public void AddCloth(Cloth cloth)
+        {
+            if (this.Capacity > this.Clothes.Count)
+            {
+                this.Clothes.Add(cloth);
+            }
+        }
+
+        public bool RemoveCloth(string color)
+        {
+            Cloth clothToRemove = this.Clothes.FirstOrDefault(c => c.Color == color);
+
+            return this.Clothes.Remove(clothToRemove);
+        }
+
+        public Cloth GetSmallestCloth()
+        {
+            return this.Clothes.OrderBy(c => c.Size).FirstOrDefault();
+        }
+
+        public Cloth GetCloth(string color)
+        {
+            return this.Clothes.FirstOrDefault(c => c.Color == color);
+        }
+
+        public string Report()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"{this.Type} magazine contains:");
+            foreach (var cloth in this.Clothes.OrderBy(c => c.Size))
+            {
+                sb.AppendLine(cloth.ToString());
+            }
+
+            return sb.ToString().Trim();
+        }
+        
+    }
+}
---
+++ ### Exam Practice ###/Clothes Magazine/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClothesMagazine
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Magazine magazine = new Magazine("Zara", 20);
+            Cloth cloth1 = new Cloth("red", 36, "dress");
+            Console.WriteLine(cloth1); //Product: dress with size 36, color red
+            magazine.AddCloth(cloth1);
+            Console.WriteLine(magazine.RemoveCloth("black")); //false
+            Cloth cloth2 = new Cloth("brown", 34, "t-shirt");
+            Cloth cloth3 = new Cloth("blue", 32, "jeans");
+            magazine.AddCloth(cloth2);
+            magazine.AddCloth(cloth3);
+            Cloth smallestCloth = magazine.GetSmallestCloth();
+            Console.WriteLine(smallestCloth); 
+            Cloth getCloth = magazine.GetCloth("brown");
+            Console.WriteLine(getCloth);
+            Console.WriteLine(magazine.Report());
+        }
+    }
+}
